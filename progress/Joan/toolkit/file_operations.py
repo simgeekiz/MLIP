70e3ad349fdb1c3b70e81e8a5d3e41e29bf13f5b
michelkok <michelkok96@gmail.com>
2018-03-02 19:21:33 +0100

    Fix inconsistent use of tabs and spaces.

--- progress/Joan/toolkit/file_operations.py
+++ progress/Joan/toolkit/file_operations.py
@@ -8,34 +8,34 @@ from numpy import genfromtxt
 
 # Takes a path to a directory and opens returns the train and test set in that directory as a pandas dataframe.
 def mnist_to_pdseries(path, scale=True):
-	train = pd.read_csv(path + 'train.csv')
-	y = train.ix[:,0]
-	train = train.drop('label',1)
-	test = pd.read_csv(path + 'test.csv')
-	if scale:
+    train = pd.read_csv(path + 'train.csv')
+    y = train.ix[:,0]
+    train = train.drop('label',1)
+    test = pd.read_csv(path + 'test.csv')
+    if scale:
         scaler = preprocessing.StandardScaler()
         train = scaler.fit_transform(train)
         test = scaler.transform(test)
-	return [train, y, test]
+    return [train, y, test]
 
 # Takes a path to a directory and opens returns the train and test set in that directory as a numpy array.
 def mnist_to_nparray(path):
-	[train, y, test] = mnist_to_pdseries(path)
-	train = train.values
-	y = y.values
-	test = test.values
-	return [train, y, test]
-	
+    [train, y, test] = mnist_to_pdseries(path)
+    train = train.values
+    y = y.values
+    test = test.values
+    return [train, y, test]
+    
 # Takes list of 28000 predictions and writes them to csv file ready for submission to Kaggle.
 def write_results(res, path='', name=''):
-	results = pd.DataFrame({'ImageId':range(1,28001), 'Label':res})
-	if name == '':
-		name = 'results'
-	results.to_csv(path + name + '.csv', index=False)
-	
+    results = pd.DataFrame({'ImageId':range(1,28001), 'Label':res})
+    if name == '':
+        name = 'results'
+    results.to_csv(path + name + '.csv', index=False)
+    
 # Takes list of categorical values and converts it to Kaggle compatible list.
 def categorical_to_class(cat):
-	classes = []
-	for c in cat:
-		classes.append(np.argmax(c))
-	return classes
+    classes = []
+    for c in cat:
+        classes.append(np.argmax(c))
+    return classes
