f9760b257feb1926e0b764a1889c7179ef5bea26
JoanRessing <hoiditbenik@live.nl>
2018-03-07 01:39:13 +0100

    Fixed problems in noise functions and computed various scores using noisy data

--- progress/Joan/conv_net.py
+++ progress/Joan/conv_net.py
@@ -4,11 +4,16 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 
+import toolkit.noise as noise
+
 print('Reading and reshaping data')
-[X_train, y_train, X_test] = mnist_to_nparray('data/')
+[X_train, y_train, X_test] = mnist_to_nparray('data/', False)
+noise.add_noisy_pixels(X_train, 0.25)
+
 X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
 X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 y_train = np_utils.to_categorical(y_train, 10)
+
 
 print('Constructing model')
 model = Sequential()
@@ -31,5 +36,6 @@ model.fit(X_train, y_train, epochs=25, batch_size=32)
 print('Making predictions')
 predictions = model.predict(X_test)
 
+output_name = input('Enter name for outputfile\n')
 print('Writing results to .csv file')
-write_results(categorical_to_class(predictions), 'results/', 'convolutional_network')
+write_results(categorical_to_class(predictions), 'results/', output_name)
